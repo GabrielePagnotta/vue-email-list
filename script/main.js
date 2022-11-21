0e9c1a71ef56a29e05ccd20403e4e9b62d887d90
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-11-21 16:53:20 +0100

    fine esercizio

--- script/main.js
+++ script/main.js
@@ -2,15 +2,26 @@ var app = new Vue({
 el: '#app',
 data: {
 
-    input:"",
+    input:[],
+   
+    
+    
 },
 mounted() {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    for (let i = 0; i < 10; i++) {
+        
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then( (response) => {
-    // console.log( response.data.response )
+     console.log( response.data.response )
      let responseData = response.data.response
-      this.input = responseData
+      this.input.push(responseData)
+      console.log(responseData)
+
     })
+
+        
+    }
+    
 
     
 },
